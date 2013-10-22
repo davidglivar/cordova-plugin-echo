46986b43c9962eb154e05d0dcb71fb9cf96efc86
David Glivar <davidglivar@gmail.com>
2013-10-22 11:28:15 -0700

    quick fix to js

--- www/echo.js
+++ www/echo.js
@@ -1,9 +1,5 @@
-cordova.define('com.davidglivar.cordova.echo', function (require, exports, module) {
-
-  window.echo = function (str, callback) {
-    cordova.exec(callback, function (err) {
-      callback('Nothing to echo');
-    }, 'Echo', 'echo', [str]);
-  };
-
-});
+window.echo = function (str, callback) {
+  cordova.exec(callback, function (err) {
+    callback('Nothing to echo');
+  }, 'Echo', 'echo', [str]);
+};
